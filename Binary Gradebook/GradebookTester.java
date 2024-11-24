877850a0515e869ccaaa0b847fb751d20254648f
heet0511 <heet_joshi@yahoo.com>
2024-11-24 01:26:17 -0600

    Update GradebookTester.java

--- Binary Gradebook/GradebookTester.java
+++ Binary Gradebook/GradebookTester.java
@@ -1,18 +1,7 @@
-////////////FILE HEADER(INCLUDE IN EVERY FILE)////////////////
-//
-// Title: Binary Gradebook
-// Course: CS300 Spring 2024
-//
+
+// Title: Binary Gradebook Tester
 // Author: Heet Joshi
 // Email: hjoshi6@wisc.edu
-// Lecturer: Mouna Kacem
-//
-////////////////// ASSISTANCE/HELP CITATIONS//////////////
-//
-// Person: No help taken
-// Online sources: No help taken
-//
-//////////////////////////////////////////////////////////////////
 
 import java.util.Iterator;
 
